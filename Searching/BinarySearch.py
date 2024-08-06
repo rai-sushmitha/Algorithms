b31803d174ad690b07302c7378c8c53c0ee9abd2
Sushmitha Rai <sushmitharai210@gmail.com>
2024-08-06 23:27:14 +0530

    Update BinarySearch.py

--- Searching/BinarySearch.py
+++ Searching/BinarySearch.py
@@ -1,3 +1,4 @@
+#Iterative method
 def BinarySearch(nums, num):
     low = 0
     high = len(nums) - 1
@@ -12,6 +13,21 @@ def BinarySearch(nums, num):
         else:
             low = mid + 1
     return -1
+
+#Recursive method
+def BinarySearchRecursion(nums, low, high, num):
+    if(high >= low):
+        mid = low + (high-low)//2
+        
+        if nums[mid] == num:
+            return mid
+
+        elif(nums[mid] > num):
+            return BinarySearchRecursion(nums, low, mid - 1, num)
+        else:
+            return BinarySearchRecursion(nums, mid + 1, high, num)
+    else:
+        return -1
+
     
-def BinarySearchRecursion(nums, low, mid, high, num):
   
